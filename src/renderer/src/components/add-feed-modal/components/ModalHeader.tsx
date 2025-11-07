4027fcd7c8aef50348a70b83d468ffc0cae9bf51
XaV1Uzz <xaviuzz@gmail.com>
2025-11-07 12:32:50 +0100

    🧹 chore: remove duplicate modal components from AddFeedModal

--- src/renderer/src/components/add-feed-modal/components/ModalHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface ModalHeaderProps {
-  title: string
-}
-
-export function ModalHeader({ title }: ModalHeaderProps): React.JSX.Element {
-  return <h3 className="font-bold text-lg">{title}</h3>
-}
